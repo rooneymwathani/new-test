3ad1de08cde90a65c25cc6d4928891527a646677
ROONEY M. LAVIA <115464810+rooneymwathani@users.noreply.github.com>
2022-10-15 02:32:41 -0700

    Add files via upload

---
+++ Maths-operations.py
@@ -0,0 +1,20 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+2 + 3
+5
+2 -3
+-1
+2 / 3
+0.6666666666666666
+2 * 3
+6
+2 % 3
+2
+2.0 % 3.0
+2.0
+4**2
+16
+5*2
+10
+2**3
+8
